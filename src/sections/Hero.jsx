8794967716ee203a9afcf3bfcbd354fdfa5b051e
Zuha <zuha.ahmed2020@gmail.com>
2025-06-04 21:50:35 -0400

    fixed the import for hero.jsx from button.jsx to Button.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,5 @@
 import { words } from '../constants/index.js'
-import Button from '../components/button.jsx'
+import Button from '../components/Button.jsx'
 import HeroExperience from '../components/HeroModels/HeroExperience.jsx'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
